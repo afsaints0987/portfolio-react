163e81f0cfe3e302b5f221a6eb1d2d7825681b99
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-06-16 19:01:35 +0800

    update meta tags

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,8 +3,14 @@ import loader2 from '../assets/loader-2.gif'
 
 const Loading = ({title}) => {
     return (
+        // <Canvas>
+        //         <ambientLight intensity={0.3}/>
+        //         <pointLight position={[10, 10, 10]}/>
+        //         <Loader/>
+        //     </Canvas>
         <div id="loading" className="text-center">
             <img src={loader} alt="loader" width="75px" className="rounded-circle"/>
+            
             <p>{title}</p>
             <img src={loader2} alt="loader2" width="50px"/>
         </div>
