925460021e5000c36e38487f58d1c028176959df
afsaints0987 <saints0987@gmail.com>
2024-10-20 13:25:07 +0800

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,30 @@ import { SocialIcon } from "react-social-icons";
 
 const Footer = () => {
   return (
-    <footer className="container text-center footer mb-2 fixed-bottom ">
-      <div className="icons container mb-4">
-        <SocialIcon
-          url="https://github.com/afsaints0987"
-          style={{ height: 40, width: 40 }}
-          target="_blank"
-          rel="noreferrer noopener"
-        />
-        <SocialIcon
-          url="https://www.linkedin.com/in/afsaints0987/"
-          style={{ height: 40, width: 40 }}
-          target="_blank"
-          rel="noreferrer noopener"
-        />
-        <SocialIcon
-          url="https://www.facebook.com/afsaintsdesign/"
-          style={{ height: 40, width: 40 }}
-          target="_blank"
-          rel="noreferrer noopener"
-        />
+    <footer className="container text-center">
+      <div className="footer mt-auto">
+        <div className="icons container mb-4">
+          <SocialIcon
+            url="https://github.com/afsaints0987"
+            style={{ height: 40, width: 40 }}
+            target="_blank"
+            rel="noreferrer noopener"
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/in/afsaints0987/"
+            style={{ height: 40, width: 40 }}
+            target="_blank"
+            rel="noreferrer noopener"
+          />
+          <SocialIcon
+            url="https://www.facebook.com/afsaintsdesign/"
+            style={{ height: 40, width: 40 }}
+            target="_blank"
+            rel="noreferrer noopener"
+          />
+        </div>
+        <p style={{ fontFamily: "Courier New" }}>v2.0.0</p>
       </div>
-      <p style={{ fontFamily: "Courier New" }}>v2.0.0</p>
     </footer>
   );
 };
