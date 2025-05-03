f98bf73303d2860f391c1cdf0c80b35e4030dc10
afsaints0987 <saints0987@gmail.com>
2025-05-03 12:35:47 +0800

    update project display

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,69 +1,61 @@
 import MotionContainer from "../components/MotionContainer";
 import { useState, useEffect } from "react";
-import * as FaIcons from "react-icons/fa"
+import * as FaIcons from "react-icons/fa";
 import { Link } from "react-router-dom";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 import axios from "axios";
 
-
 const Project = () => {
-  const [projects, setProjects] = useState([])
-  const [project, setProject] = useState([])
-  const [selectedProject, setSelectedProject] = useState(0)
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     const fetchProjects = async () => {
-      const projectData  = await axios.get("projects.json")
-      const projectList = projectData.data
-      const projectItems = projectList.projects
-      setProjects(projectItems)
-
-      const defaultProjectId = projectItems.length > 0 ? projectItems[0].id : 0;
-      setSelectedProject(defaultProjectId)
-    }
+      const projectData = await axios.get("projects.json");
+      const projectList = projectData.data.projects || [];
+      setProjects(projectList);
+    };
     fetchProjects();
-  },[])
+  }, []);
 
-  const handleProject = (id = selectedProject) => {
-    const projectSelected = projects.filter(proj => proj.id === id)
-    setProject(projectSelected)
-  }
-  
-  useEffect(() => {
-    handleProject()
-  },[projects])
-  
   return (
     <MotionContainer>
-      <div className="container mt-4">
+      <div className="container mt-5">
         <h2 className="text-center mb-4">Projects</h2>
-        <div className="project-container px-4 py-2 d-flex flex-column-reverse flex-lg-row">
-          <div className="col-lg-3">
-            <div>
-              {projects.map((proj) => (
-                  <ul className="proj-list list-group-flush" key={proj.id}>
-                    <li className="list-group-item mt-2" >
-                      <button className="btn btn-sm proj-select text-start-lg" onClick={() => handleProject(proj.id)}>{proj.title}</button>
-                    </li>
-                  </ul>
-              ))}
+        <div className="row g-4">
+          {projects.map((proj) => (
+            <div key={proj.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
+              <motion.div
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
+                className="card h-100 shadow-sm"
+              >
+                <a
+                  href={proj.url}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="m-0"
+                >
+                  <img
+                    src={proj.image}
+                    alt={proj.title}
+                    className="card-img-top img-fluid"
+                    loading="lazy"
+                  />
+                </a>
+                <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">{proj.title}</h5>
+                  <p className="card-text">{proj.description}</p>
+                  <Link
+                    to={`/project/${proj.id}`}
+                    className="mt-auto btn btn-outline-primary btn-sm w-100"
+                  >
+                    Full Details <FaIcons.FaAngleDoubleRight />
+                  </Link>
+                </div>
+              </motion.div>
             </div>
-          </div>
-            <div className="proj-details col-lg-9">
-              {project.map(proj => (
-                <div className="text-center d-flex" key={proj.id}>
-                    <motion.div initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    exit={{opacity: 0}}>
-                    <a href={proj.url} target="_blank" rel="noreferrer noopener"><img src={proj.image} alt={proj.title} className="rounded-3 mb-4 shadow img-fluid w-75" loading="lazy"/></a>
-                    <h4>{proj.title}</h4>
-                    <p>{proj.description}</p>
-                    <Link to={`/project/${proj.id}`}>Full Details <FaIcons.FaAngleDoubleRight/></Link>                  
-                    </motion.div>
-                  </div>  
-              ))}
-            </div>
-
+          ))}
         </div>
       </div>
     </MotionContainer>
@@ -71,4 +63,3 @@ const Project = () => {
 };
 
 export default Project;
-
